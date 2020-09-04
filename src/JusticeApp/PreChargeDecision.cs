9cebd0530748c6f72ddad2937bf389a9907352b6
Barry Postma <bpostma@thoughtworks.com>
2020-09-04 17:04:25 +0100

    Exercise 1 - RED - Implement missing class and functions - tests still fail

--- src/JusticeApp/PreChargeDecision.cs
+++ src/JusticeApp/PreChargeDecision.cs
@@ -1,8 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 namespace HarmelLaw.JusticeApp
 {
     public class PreChargeDecision
     {
+        private Dictionary<Suspect, OffenceAdvice> _offenceAdvice = new Dictionary<Suspect, OffenceAdvice>();
+
+        public void RecordAlternativeOffenceAdvice(Suspect suspect, OffenceAdvice advice)
+        {
+        }
+
+        public OffenceAdvice GetOffenceAdviceFor(Suspect suspect)
+        {
+            return null;
+        }
+
     }
 }
